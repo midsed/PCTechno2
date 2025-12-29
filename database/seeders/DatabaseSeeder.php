be715074bf4c4071b65a9046a15c997424b98073
Blaze816 <ejpangilinan816.ejp@gmail.com>
2025-12-30 04:55:42 +0800

    Refactor DatabaseSeeder to streamline user creation and remove unused code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,33 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
-{
-    use WithoutModelEvents;
+class DatabaseSeeder extends Seeder {
+    public function run(): void {
+        User::create( [
+            'full_name' => 'Admin User',
+            'username'  => 'admin',
+            'email'     => 'admin@example.com',
+            'role'      => 'admin',
+            'password'  => Hash::make( 'password' ),
+        ] );
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+        User::create( [
+            'full_name' => 'Employee User',
+            'username'  => 'employee',
+            'email'     => 'employee@example.com',
+            'role'      => 'employee',
+            'password'  => Hash::make( 'password' ),
+        ] );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::create( [
+            'full_name' => 'Cashier User',
+            'username'  => 'cashier',
+            'email'     => 'cashier@example.com',
+            'role'      => 'cashier',
+            'password'  => Hash::make( 'password' ),
+        ] );
     }
 }
